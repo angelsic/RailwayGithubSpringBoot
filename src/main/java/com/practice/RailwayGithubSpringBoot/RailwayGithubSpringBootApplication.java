7fa28f91c7bf20df00a74e8eb9ca83db7f18294f
angelsic <newangeled@gmail.com>
2024-05-21 20:39:35 -0600

    feat(backend): Create Get endpoint and return my name

---
+++ src/main/java/com/practice/RailwayGithubSpringBoot/RailwayGithubSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.practice.RailwayGithubSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayGithubSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayGithubSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/RailwayGithubSpringBoot/RailwayGithubSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.RailwayGithubSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayGithubSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
